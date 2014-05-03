5798baa56e46d01e6b48bed166332f680c33842a
George Vigelette <gvigelet@vigeltek.com>
2014-05-03 08:04:27 -0400

    initial Check in

---
+++ CrypTick/CrypTick/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CrypTick
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
